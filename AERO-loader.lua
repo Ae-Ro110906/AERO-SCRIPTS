8d92fbadfa9980510ca957e4aa2325d031bd8669
Ae-Ro110906 <ayseyviyerentena@gmail.com>
2026-06-02 06:04:26 +0800

    Update AERO-loader.lua

--- AERO-loader.lua
+++ AERO-loader.lua
@@ -1,1 +1,20 @@
-loadstring(game:HttpGet("https://jsdelivr.net"))()
+-- Aero Scripts Hub Intro Loader Template
+local StarterGui = game:GetService("StarterGui")
+
+-- Send a clean loading notification box into the bottom corner
+StarterGui:SetCore("SendNotification", {
+    Title = "Aero Scripts",
+    Text = "Authenticating client data... Please wait.",
+    Duration = 5
+})
+
+task.wait(1)
+
+-- Safely download and run your massive Main script file using the instant proxy bypass
+loadstring(game:HttpGet("https://githack.com"))()
+
+StarterGui:SetCore("SendNotification", {
+    Title = "Aero Scripts",
+    Text = "Framework successfully executed!",
+    Duration = 3
+})
